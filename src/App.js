d9ab0999c3b2af27b8adca00349d600a3e0d6c97
Chris Spoonts <cspoonts@protonmail.com>
2022-08-11 22:38:44 -0500

    Update Icon

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 function App() {
 	return (
-		<div className='textaligncenter'>
-			<h1>
-				Future home of <strong>Fab²</strong>
-			</h1>
+		<div className='centraliser'>
+			<img src='https://dl.dropbox.com/s/1x15avtq1rkmzer/iconsimple.svg?dl=0?raw+1' alt='Fab²' />
+			<br />
+			<br />
 			<div>
-				fab squared a <strong>DARK WATER CUSTOMS</strong> company
-			</div>
-			<img src='https://dl.dropbox.com/s/2ec1gyh5sqiu87u/plasmatorch.png?dl=0?raw=1' alt='Fab²' />
-			<div>
-				If you are a <strong>visitor</strong>, check back soon.
-			</div>
-			<div>
-				<a href='https://www.darkwatercustoms.com'>www.darkwatercustoms.com</a>
+				Future home of a <strong>DARK WATER CUSTOMS</strong>
+				{' company '}
+				<a href='http://www.darkwatercustoms.com'>www.darkwatercustoms.com</a>
 			</div>
 		</div>
 	)
