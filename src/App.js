2d741b33b89b6ebafa923440b57419157432aa4e
Chris Spoonts <cspoonts@protonmail.com>
2022-08-11 21:38:34 -0500

    update with link

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ function App() {
 	return (
 		<div className='textaligncenter'>
 			<h1>
-				Future home of <strong>FAB²</strong>
+				Future home of <strong>Fab²</strong>
 			</h1>
 			<div>
-				a <strong>DARK WATER CUSTOMS</strong> company
+				fab squared a <strong>DARK WATER CUSTOMS</strong> company
 			</div>
 			<img src='https://dl.dropbox.com/s/2ec1gyh5sqiu87u/plasmatorch.png?dl=0?raw=1' alt='Fab²' />
 			<div>
 				If you are a <strong>visitor</strong>, check back soon.
+			</div>
+			<div>
+				<a href='https://www.darkwatercustoms.com'>www.darkwatercustoms.com</a>
 			</div>
 		</div>
 	)
